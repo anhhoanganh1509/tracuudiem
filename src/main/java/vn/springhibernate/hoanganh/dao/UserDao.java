da673fe7c85d9544ddd6af0f79370636977dbd52
shophoanganhsport <anhhoanganh1509>
2018-12-26 22:24:16 +0700

    seven commit

--- src/main/java/vn/springhibernate/hoanganh/dao/UserDao.java
+++ src/main/java/vn/springhibernate/hoanganh/dao/UserDao.java
@@ -6,7 +6,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,18 +15,15 @@ import vn.springhibernate.hoanganh.model.Users;
 
 @Repository
 @Transactional(rollbackFor = Exception.class)
-public class UserDao{
+public class UserDao {
 
 	@Autowired
-	private SessionFactory sessionFactory;
+	private SessionFactory sessionFactory;	
 
-	@Autowired
-	private JavaMailSender mailSender;
-
-	public void CreateNewUser(final Users u){
+	public void CreateNewUser(final Users u) {
 		Session session = this.sessionFactory.getCurrentSession();
 		String pass = String.valueOf(new Date().getTime());
-		u.setUsername(u.getUsername());		
+		u.setUsername(u.getUsername());
 		u.setPassword(pass.substring(8));
 		u.setEnabled(0);
 		u.setEmail(u.getEmail());
@@ -38,21 +34,21 @@ public class UserDao{
 		ur.setUsername(u.getUsername());
 		ur.setRole("ROLE_ADMIN");
 		session.save(ur);
-		
+
 		String subject = "User Activation Account";
-		String message = "Please click on the following link to active your account."
-				+"\n"+"https://tracuudiem.herokuapp.com/activeUser/"+u.getUsername()+"\n\n"
-				+"- Username:"+u.getUsername()+"\n"+"- Password:"+pass.substring(8);
-		
+		String message = "Please click on the following link to active your account." + "\n"
+				+ "https://tracuudiem.herokuapp.com/activeUser/" + u.getUsername() + "\n\n" + "- Username:"
+				+ u.getUsername() + "\n" + "- Password:" + pass.substring(8);
+
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setTo(u.getEmail());
 		email.setSubject(subject);
-		email.setText(message);	
+		email.setText(message);
 		try {
-			mailSender.send(email);
-		}catch(Exception ex) {
-			ex.printStackTrace();		
-		}		
+			//mailSender.send(email);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public Users finbyId(final String id) {
@@ -61,41 +57,41 @@ public class UserDao{
 	}
 
 	public void ActiveUsers(Users u) {
-		if(u.getEnabled() == 0) {
+		if (u.getEnabled() == 0) {
 			Session session = this.sessionFactory.getCurrentSession();
 			u.setEnabled(1);
-			u.setNgaysua(new Date());	
+			u.setNgaysua(new Date());
 			session.update(u);
 		}
 	}
-	
+
 	public Long countCheckMail(String email) {
 		Session session = this.sessionFactory.getCurrentSession();
-		String sql = "Select count(o.username) from " 
-				+Users.class.getName() + " o where o.daxoa = 0 and o.email like '"+email+"'";
-		Long value = (Long)session.createQuery(sql).uniqueResult();
-        if (value == null) {
-            return 0L;
-        }
-        return value;
+		String sql = "Select count(o.username) from " + Users.class.getName()
+				+ " o where o.daxoa = 0 and o.email like '" + email + "'";
+		Long value = (Long) session.createQuery(sql).uniqueResult();
+		if (value == null) {
+			return 0L;
+		}
+		return value;
 	}
-	
+
 	public Long countCheckMaSo(String username) {
 		Session session = this.sessionFactory.getCurrentSession();
-		String sql = "Select count(o.username) from " 
-				+Users.class.getName() + " o where o.daxoa = 0 and o.username like '"+username+"'";
-		Long value = (Long)session.createQuery(sql).uniqueResult();
-        if (value == null) {
-            return 0L;
-        }
-        return value;
-	}	
+		String sql = "Select count(o.username) from " + Users.class.getName()
+				+ " o where o.daxoa = 0 and o.username like '" + username + "'";
+		Long value = (Long) session.createQuery(sql).uniqueResult();
+		if (value == null) {
+			return 0L;
+		}
+		return value;
+	}
 
 	public Student finbyStudentId(final String id) {
 		Session session = this.sessionFactory.getCurrentSession();
 		return session.get(Student.class, id);
 	}
-	
+
 	public void UpdatePass(final String newPass, final String id) {
 		Session session = this.sessionFactory.getCurrentSession();
 		Users u = this.finbyId(id);
@@ -103,15 +99,16 @@ public class UserDao{
 		u.setNgaysua(new Date());
 		session.update(u);
 		String subject = "Change Password";
-		String message = "The change password process has completed successfully."
-				+"\n"+"- Username:"+u.getUsername()+"\n"+"- Password:"+newPass;		
+		String message = "The change password process has completed successfully." + "\n" + "- Username:"
+				+ u.getUsername() + "\n" + "- Password:" + newPass;
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setTo(u.getEmail());
 		email.setSubject(subject);
-		email.setText(message);		
+		email.setText(message);
 		try {
-			mailSender.send(email);
-		}catch(Exception ex) {
+			MailUtil.send("hoangsinaran5@gmail.com", u.getEmail(), subject, "");			
+			//mailSender.send(email);
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
